f1c558f4fc66c577fe23bdbdef0cadc86f894a90
Teddy Williams <theodore.f.williams@gmail.com>
2023-12-11 19:04:40 -0700

    add initial projects and experience routes. Add button and link CSS components. Various text + grammar improvements.

--- app/web/html/routes/landing.py
+++ app/web/html/routes/landing.py
@@ -13,13 +13,45 @@ router = APIRouter(tags=["landing"])
 async def landing(
     request: Request,
 ) -> _TemplateResponse:
-    """Return the portfolio landing page."""
+    """Return the portfolio landing (about) page."""
     headers = {
         "HX-Replace-Url": str(request.url_for("html:landing")),
         "HX-Refresh": "true",
     }
     return templates.TemplateResponse(
         "main/about/about.html",
+        {"request": request},
+        headers=headers,
+    )
+
+
+@router.get("/projects", response_model=None)
+async def projects(
+    request: Request,
+) -> _TemplateResponse:
+    """Return the portfolio landing page."""
+    headers = {
+        "HX-Replace-Url": str(request.url_for("html:projects")),
+        "HX-Refresh": "true",
+    }
+    return templates.TemplateResponse(
+        "main/projects/projects.html",
+        {"request": request},
+        headers=headers,
+    )
+
+
+@router.get("/experience", response_model=None)
+async def experience(
+    request: Request,
+) -> _TemplateResponse:
+    """Return the portfolio landing page."""
+    headers = {
+        "HX-Replace-Url": str(request.url_for("html:experience")),
+        "HX-Refresh": "true",
+    }
+    return templates.TemplateResponse(
+        "main/experience/experience.html",
         {"request": request},
         headers=headers,
     )
