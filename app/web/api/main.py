7beb86fe9439eadc50133a21d2a15f0a43f42bb2
Teddy Williams <theodore.f.williams@gmail.com>
2024-03-09 09:38:06 -0700

    use the 'register_error_handlers' method for adding error handlers to API

--- app/web/api/main.py
+++ app/web/api/main.py
@@ -1,9 +1,8 @@
 """main: FastAPI application instance for the API."""
 
-import importlib
-
 from fastapi import FastAPI
 
+from app.web.api.error_handlers import register_error_handlers
 from app.web.api.routes import auth, users
 
 app = FastAPI()
@@ -11,7 +10,4 @@ app = FastAPI()
 for route in (auth, users):
     app.include_router(route.router)
 
-# Import error handlers after app is defined
-# Can't use standard import name because `app` above conflicts with
-# the `app` package
-importlib.import_module("app.web.api.error_handlers")
+register_error_handlers(app)
