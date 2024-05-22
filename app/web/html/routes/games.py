2ce62c3aee0a3eaa7c173d916592451484892e94
Teddy Williams <theodore.f.williams@gmail.com>
2024-05-22 16:50:39 -0600

    implement connect 4 basics

---
+++ app/web/html/routes/games.py
@@ -0,0 +1,16 @@
+"""games: routes for games."""
+
+from fastapi import APIRouter, Request
+from starlette.templating import _TemplateResponse
+
+from app import constants
+from app.web.html.const import templates
+
+# ----------- Routers -----------
+router = APIRouter(tags=["games"])
+
+
+@router.get("/connect-4", response_model=None)
+async def login_get(request: Request) -> _TemplateResponse:
+    """Return the connect 4 game page."""
+    return templates.TemplateResponse("projects/connect4.html", {constants.REQUEST: request})
